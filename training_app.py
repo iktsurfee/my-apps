9874e0a35beb1e6fa6c63bc26680f44d731f50d0
iktsurfee <ikkato0@gmail.com>
2026-04-08 18:12:23 +0900

    Update training_app.py

--- training_app.py
+++ training_app.py
@@ -517,4 +517,6 @@ loadAll();
 </html>"""
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    import os
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port)
