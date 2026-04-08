e4aebb8b71ebfe748ece556180c8e926c7cd71e0
iktsurfee <ikkato0@gmail.com>
2026-04-08 18:11:40 +0900

    Update wbs_app.py

--- wbs_app.py
+++ wbs_app.py
@@ -635,5 +635,5 @@ loadAll();
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
